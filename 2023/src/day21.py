90fb84696c852900f0ee052c6f49deb999450772
Florence Yernaux <florence.yernaux@ulb.ac.be>
2023-12-21 06:56:39 +0100

    day21 part2 still too slow

--- 2023/src/day21.py
+++ 2023/src/day21.py
@@ -9,14 +9,49 @@ import numpy as np
 def add(a, b):
     return (a[0]+b[0], a[1]+b[1])
 
+
 def outOfBounds(pos, map_shape):
     for i in range(2):
         if pos[i] < 0 or pos[i] >= map_shape[i]:
             return True
     return False
 
+
 def infinitePos(pos, map_shape):
-    return (pos[0]%map_shape[0], pos[1]%map_shape[1])
+    return (pos[0] % map_shape[0], pos[1] % map_shape[1])
+
+
+def solve(garden, max_step, infinite_map):
+    tmp = np.where(garden == 2)
+    start_pos = (tmp[0][0], tmp[1][0])
+    # print(f"Start pos at {start_pos}, garden shape of {garden.shape}")
+
+    item = (start_pos, 0)
+    visited_pos = {}
+    items = queue.Queue()
+    items.put(item)
+
+    # count_possibilities = set()
+    count_possibilities = 0
+
+    while not items.empty():
+        curr, steps = items.get()
+        if steps == max_step:
+            count_possibilities += 1
+            # count_possibilities.add(curr)
+            continue
+        steps += 1
+        for dir in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            new_pos = add(curr, dir)
+            if not infinite_map and outOfBounds(new_pos, garden.shape):
+                continue
+            if garden[infinitePos(new_pos, garden.shape)] == 1:
+                continue
+            if (new_pos in visited_pos and visited_pos[new_pos] < steps) or (new_pos not in visited_pos):
+                visited_pos[new_pos] = steps
+                items.put((new_pos, steps))
+    return count_possibilities  # len(count_possibilities)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Day21")
@@ -29,47 +64,19 @@ def main():
         file = "./src/files/day21ex.txt"
 
     garden = []
-    corr = {".": 0 , "#": 1 , 'S':2}
+    corr = {".": 0, "#": 1, 'S': 2}
     with open(file, "r") as f:
         for il, line in enumerate(f.readlines()):
             garden.append(np.array([corr[x] for x in line.strip()]))
-    #print(garden)
+    # print(garden)
     garden = np.array(garden)
-    print(garden)
-    tmp = np.where(garden == 2)
-    start_pos = (tmp[0][0], tmp[1][0])
-    print(start_pos)
-    print(garden[start_pos])
-    
-    max_step = 64
-    max_step = 500 #26501365
 
-    item = {"curr": start_pos, "steps": 0, "prev": {}}
-    visited_pos = {}
-    items = queue.Queue()
-    items.put(item)
-    
-    count_possibilities = set()
-    while not items.empty():
-        item = items.get()
-        if item["steps"] == max_step:
-            count_possibilities.add(item["curr"])
-            continue
-        item["steps"] += 1
-        for dir in [(0,1),(1,0),(0,-1), (-1,0)]:
-            new_item = copy.copy(item)
-            new_pos = add(item["curr"], dir)
-            # if outOfBounds(new_pos, garden.shape):
-            #     continue
-            if garden[infinitePos(new_pos, garden.shape)] == 1:
-                continue
-            if (new_pos in visited_pos and visited_pos[new_pos] < new_item["steps"]) or (new_pos not in visited_pos):
-                new_item["curr"] = new_pos
-                visited_pos[new_pos] = new_item["steps"]
-                items.put(new_item)
-    print(f"Part 1 {len(count_possibilities)}")
+    print(
+        f"Part 1: {solve(garden, 6 if args.ex else 64, infinite_map=False)} steps")
 
-    
+    print(
+        f"Part 2: {solve(garden, 50 if args.ex else 26501365, infinite_map=True)} steps")
+
 
 if __name__ == "__main__":
     main()
